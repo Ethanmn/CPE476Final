9687dffec6788283a23ceeb5f7b7edd82539ede3
Christopher Hebert <hebert.christopherj@gmail.com>
2014-04-20 12:09:07 -0700

    deer accelerates to walking speed

--- src/deer.cpp
+++ src/deer.cpp
@@ -4,7 +4,8 @@
 #include "graphics/uniform_location_map.h"
 
 const float kSpeed = 0.005f;
-const float kFriction = 0.05f;
+const float kFriction = 0.005f;
+const float kAcceleration = 0.00007f;
 
 void Deer::draw(Shader& shader, const UniformLocationMap& model_locations) const {
    const glm::mat4 model_matrix(glm::translate(glm::mat4(1.0f), position_));
@@ -14,22 +15,28 @@ void Deer::draw(Shader& shader, const UniformLocationMap& model_locations) const
 
 void Deer::step(units::MS dt, const Camera& camera) {
    if (walk_direction_ == WalkType::NONE && strafe_direction_ == StrafeType::NONE) {
-      velocity_ -= velocity_ * kFriction;
-      if (glm::length(velocity_) < kSpeed) {
+      velocity_ -= velocity_ * (kFriction * dt);
+      if (glm::length(velocity_) < kSpeed / 4.0f) {
          velocity_ = glm::vec3(0.0f);
       }
    } else {
-      if (walk_direction_ == WalkType::FORWARD)
-         velocity_ = camera.getCamForwardVec();
-      else if (walk_direction_ == WalkType::BACKWARD)
-         velocity_ = -camera.getCamForwardVec();
-      if (strafe_direction_ == StrafeType::LEFT)
-         velocity_ += camera.getCamLeftVec();
-      else if (strafe_direction_ == StrafeType::RIGHT)
-         velocity_ -= camera.getCamLeftVec();
-      velocity_ = glm::normalize(velocity_) * (kSpeed * dt);
+      glm::vec3 acceleration(0.0f);
+      if (walk_direction_ == WalkType::FORWARD) {
+         acceleration = camera.getCamForwardVec();
+      } else if (walk_direction_ == WalkType::BACKWARD) {
+         acceleration = -camera.getCamForwardVec();
+      }
+      if (strafe_direction_ == StrafeType::LEFT) {
+         acceleration += camera.getCamLeftVec();
+      } else if (strafe_direction_ == StrafeType::RIGHT) {
+         acceleration -= camera.getCamLeftVec();
+      }
+      velocity_ += glm::normalize(acceleration) * (kAcceleration * dt);
+      if (glm::length(velocity_) > kSpeed) {
+         velocity_ = glm::normalize(velocity_) * kSpeed;
+      }
    }
-   position_ += velocity_;
+   position_ += velocity_ * static_cast<float>(dt);
 }
 
 void Deer::walkForward() {
