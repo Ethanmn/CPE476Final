0a073f48a948114df056e633c5736fad01815046
katelynhicks <katiehicks94@gmail.com>
2014-04-20 22:26:34 -0700

    Trying to put a ground plane

---
+++ src/GroundPlane.cpp
@@ -0,0 +1,58 @@
+#include "GroundPlane.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "graphics/Shader.h"
+#include "graphics/Shaders.h"
+
+const std::vector<float> ground_vertices{
+   -0.5, -0.5, 0.0,
+   0.5, -0.5, 0.0,
+   -0.5, 0.5, 0.0,
+   0.5, 0.5, 0.0,
+};
+const std::vector<float> ground_normals{
+   0, 0, 1,
+   0, 0, 1,
+   0, 0, 1,
+   0, 0, 1,
+};
+const std::vector<float> ground_tex_coord{
+   0.0, 0.0,
+   1.0, 0.0,
+   0.0, 1.0,
+   1.0, 1.0
+};
+
+const std::vector<unsigned short> ground_indices{
+   0, 2, 1, 3, 1, 2
+};
+
+GroundPlane::GroundPlane(Shaders& shaders) :
+   mesh_{
+      IndexBufferObject::create(ground_indices),
+      {
+         ArrayBufferObject::create(
+            ground_vertices,
+            shaders.getAttributes(Attribute::VERTEX),
+            3),
+         ArrayBufferObject::create(
+            ground_normals,
+            shaders.getAttributes(Attribute::NORMAL),
+            3),
+         ArrayBufferObject::create(
+            ground_tex_coord,
+            shaders.getAttributes(Attribute::TEX_COORD),
+            3),
+      }
+   }
+/*
+   model_matrix_(shaders,
+         glm::rotate(glm::scale(glm::mat4(), glm::vec3(20.0f)), 90.0f, glm::vec3(1, 0, 0))),
+   blue_uniform_(shaders.getUniforms(Uniform::BLUE)) {*/
+{}
+
+void GroundPlane::draw(Shader& shader, const glm::mat4& view) {
+   shader.sendUniform(blue_uniform_, 0.0f);
+   //shader.drawMesh(model_matrix_.calculateAffineUniforms(view), mesh_);
+}
---
+++ src/GroundPlane.h
@@ -0,0 +1,27 @@
+#ifndef GROUND_PLANE_H_
+#define GROUND_PLANE_H_
+
+//#include "bounds.h"
+//#include "graphics/gl_adapters/uniform_matrix.h"
+#include "graphics/mesh.h"
+#include "glm/glm.hpp"
+//#include "graphics/model_view_uniform_matrix.h"
+
+struct Shader;
+struct Shaders;
+
+struct GroundPlane {
+   GroundPlane(Shaders& shaders);
+
+   void draw(Shader& shader, const glm::mat4& view);
+
+   //Bounds x_bounds() const { return Bounds(-10, 10); }
+   //Bounds z_bounds() const { return Bounds(-10, 10); }
+
+  private:
+   Mesh mesh_;
+   //ModelViewUniformMatrix model_matrix_;
+   GLUniformLocationMap blue_uniform_;
+};
+
+#endif // GROUND_PLANE_H_
