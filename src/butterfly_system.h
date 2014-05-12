8c5183c611da38c0129b265768379063a3d236ca
Ethan Nakashima <ethanmn@hotmail.com>
2014-05-11 19:12:35 -0700

    fuck if I know

---
+++ src/butterfly_system.h
@@ -0,0 +1,28 @@
+#ifndef BUTTERFLY_SYSTEM_H_
+#define BUTTERFLY_SYSTEM_H_
+
+#include "particle.h"
+#include "units.h"
+#include "graphics/location_maps.h"
+#include "graphics/assimp/mesh_loader.h"
+
+struct ButterflySystem {
+   ButterflySystem(const glm::vec3& origin, int numParticles,
+	       const AttributeLocationMap& attribute_location_map_, MeshLoader& mesh_loader_,
+	       const Texture& texture);
+
+   void step(units::MS dt);
+
+   void draw(Shader& shader,
+         const UniformLocationMap& uniform_location_map,
+         const glm::mat4& view_matrix);
+   
+   private:
+      std::vector<Particle> particles_;
+      glm::vec3 origin_;
+      float scale_;
+      glm::vec3 velocity_;
+      glm::vec3 acceleration_;
+};
+
+#endif // BUTTERFLY_SYSTEM_H_
