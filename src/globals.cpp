0044d539208289247434769d29ae51c06c9f7dc0
Katelyn Hicks <katiehicks94@gmail.com>
2014-05-21 23:17:34 -0700

    Ifdef Apple in globals for Katelyn

--- src/globals.cpp
+++ src/globals.cpp
@@ -3,4 +3,8 @@
 const int kScreenWidth = 640;
 const int kScreenHeight = 480;
 
+#ifdef __APPLE__
+const bool kDebugUseDeerModels = true; //For Katelyn
+#else
 const bool kDebugUseDeerModels = false;
+#endif
