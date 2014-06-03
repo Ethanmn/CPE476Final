12923a403ce235677b8def8faf725a8db4faef43
Christopher Hebert <hebert.christopherj@gmail.com>
2014-06-02 22:54:24 -0700

    trees cast shadow by default

--- src/TreeGenerator.cpp
+++ src/TreeGenerator.cpp
@@ -10,8 +10,12 @@ const int TREE_DENSITY = 4; //Higher numbers here will mean less trees.
 const int TREE_SCALE = 5;
 
 TreeGenerator::TreeGenerator(const Mesh& mesh) :
-   draw_template_({ShaderType::TEXTURE, mesh, 
-         Texture(TextureType::TREE, DIFFUSE_TEXTURE), boost::none, EffectSet({EffectType::CASTS_REFLECTION})
+   draw_template_({
+         ShaderType::TEXTURE,
+         mesh,
+         Texture(TextureType::TREE, DIFFUSE_TEXTURE),
+         boost::none,
+         EffectSet({EffectType::CASTS_SHADOW, EffectType::CASTS_REFLECTION})
          })
 {
    draw_template_.mesh.material = Material(glm::vec3(1.2) * glm::vec3(0.45, 0.24, 0.15));
