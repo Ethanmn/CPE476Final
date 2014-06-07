d0826d085bebd6230e40d4051359cb60d6cbecd4
Katie <katharina.theresa.keim@gmail.com>
2014-06-07 13:54:19 -0700

    Butterfly variance.

--- src/particle.cpp
+++ src/particle.cpp
@@ -10,17 +10,29 @@ void Particle::step(units::MS dt) {
 }
 
 glm::mat4 Particle::calculateModel() const {
-      const glm::mat4 translate(glm::translate(
-            glm::mat4(),
-            position_));
-      const glm::mat4 scale(glm::scale(
-            glm::mat4(),
-            glm::vec3(scale_)));
-      const glm::mat4 rotate(glm::rotate(
-            glm::mat4(),
-            rotate_,
-            glm::vec3(0, 1, 0)));
-      return glm::mat4(translate * scale * rotate);
+   const glm::mat4 translate(
+      glm::translate(
+         glm::mat4(),
+         position_
+      )
+   );
+
+   const glm::mat4 scale(
+      glm::scale(
+         glm::mat4(),
+         glm::vec3(scale_)
+      )
+   );
+      
+   const glm::mat4 rotate(
+      glm::rotate(
+         glm::mat4(),
+         rotate_,
+         glm::vec3(0, 1.0f, 0)
+      )
+   );
+      
+   return glm::mat4(translate * scale * rotate);
 }
 
 void Particle::setAccel(float x, float y, float z) {
