30d882bd78bdeb4d9e1003a432b22d9fd1d60b5a
Katie <katharina.theresa.keim@gmail.com>
2014-06-07 13:39:47 -0700

    Rock variance.

--- src/Rock.cpp
+++ src/Rock.cpp
@@ -1,29 +1,36 @@
+/* 
+   Rock.cpp
+   Katie Keim
+   Deer - CPE 476
+*/
 #include "Rock.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include "graphics/shader.h"
 #include "graphics/material.h"
 #include "sound_engine.h"
 
-Rock::Rock(const Mesh& mesh, const glm::vec3& position, const GroundPlane& ground, 
+Rock::Rock(const Mesh& mesh, const glm::vec3& position, float angleOffset, const GroundPlane& ground, 
       float scale) :
-   rotate_(0.0f),
    bounding_rectangle_(BoundingRectangle(
             glm::vec2(position.x, position.z),
-            glm::vec2(2.0f),
+            glm::vec2(2.0f * scale),
             0.0f)),
+   scale(scale),
    translate_scale_(
          glm::translate(glm::mat4(), glm::vec3(position.x, ground.heightAt(position) - mesh.min.y, position.z)) *
          glm::scale(glm::mat4(), glm::vec3(scale))),
    default_model_(
-         translate_scale_ *
+      translate_scale_ *
+      glm::rotate(
          glm::rotate(
-            glm::rotate(
-               glm::mat4(),
-               rotate_,
-               glm::vec3(0, 1, 0)
-               ),
-            -90.0f,
-            glm::vec3(1, 0, 0)))
+            glm::mat4(),
+            angleOffset,
+            glm::vec3(0, 1.0f, 0)
+            ),
+         -90.0f,
+         glm::vec3(1.0f, 0, 0)
+      )
+   )
 {}
 
 glm::mat4 Rock::calculateModel() const {
@@ -31,7 +38,7 @@ glm::mat4 Rock::calculateModel() const {
 } 
 
 bool Rock::isBlocker() {
-   return true;
+   return scale > 0.5;
 }
 
 void Rock::performObjectHit(SoundEngine& sound_engine) {
